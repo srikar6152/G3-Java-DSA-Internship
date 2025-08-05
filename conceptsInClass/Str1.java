a8e96fb4143db71d5ac3e4431d9512edccabf64b
srikar6152 <107166057+srikar6152@users.noreply.github.com>
2025-08-06 05:18:20 +0530

    Add files via upload

---
+++ conceptsInClass/Str1.java
@@ -0,0 +1,11 @@
+package dsaday1;
+
+public class Str1 {
+	public static void main(String[] args) {
+		String s1 = "katakam";
+		String s2 = "katakam";
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
